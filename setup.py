f2af1e780bf71a821458c6121f936fa90179c206
Harris Schneiderman <harris.schneiderman@deque.com>
2017-06-06 15:09:33 -0400

    1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name     = "keycloak",
-  version      = "0.1.1",
+  version      = "1.0.0",
   description  = "PostgreSQL foreign data wrapper for keycloak's REST api",
   author       = "Harris Schneiderman",
   author_email = "harris.schneiderman@deque.com",
